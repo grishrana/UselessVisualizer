e9559483d92ce7f7cb33691d7f95aec7f2028ea4
Grish Rana <grishrana6@gmail.com>
2024-08-13 08:46:04 +0545

    Added Nav bar

--- home.py
+++ home.py
@@ -1,3 +1,38 @@
+from numpy import True_
 import streamlit as st
 
-st.title("Home Page")
+about_page = st.Page(
+    page="pages/About.py",
+    title="Devs",
+    icon="👩‍💻",
+)
+
+stock_page = st.Page(
+    page="pages/stockVisualize.py",
+    title="Stock Visualzer",
+    icon="📈",
+    default=True,
+)
+
+world_pop = st.Page(
+    page="pages/worldPopVisual.py",
+    title="World Population",
+    icon="🌍",
+)
+
+user_data = st.Page(
+    page="pages/visualizeUserData.py",
+    title="Visualize Your Data",
+    icon="👤",
+)
+
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [stock_page, world_pop, user_data],
+    }
+)
+
+st.sidebar.text("Made by grishrana and Sushil346")
+
+pg.run()
